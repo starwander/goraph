ec3278dc8fb58bc427152e14137b76e6a7d1ae33
WeiJie Zhuang <zhuangwj@gmail.com>
2016-10-21 13:15:38 +0800

    add dijkstra shortest path algorithm
    Signed-off-by: WeiJie Zhuang <zhuangwj@gmail.com>

---
+++ dijkstra.go
@@ -0,0 +1,70 @@
+// Copyright(c) 2016 Ethan Zhuang <zhuangwj@gmail.com>.
+
+package goraph
+
+import (
+	"fmt"
+	"github.com/EthanZhuang/GoFibonacciHeap"
+	"math"
+)
+
+// https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+//1  function Dijkstra(Graph, source):
+//2      dist[source] ← 0                                    // Initialization
+//3
+//4      create vertex set Q
+//5
+//6      for each vertex v in Graph:
+//7          if v ≠ source
+//8              dist[v] ← INFINITY                          // Unknown distance from source to v
+//9              prev[v] ← UNDEFINED                         // Predecessor of v
+//10
+//11         Q.add_with_priority(v, dist[v])
+//12
+//13
+//14     while Q is not empty:                               // The main loop
+//15         u ← Q.extract_min()                             // Remove and return best vertex
+//16         for each neighbor v of u:                       // only v that is still in Q
+//17             alt = dist[u] + length(u, v)
+//18             if alt < dist[v]
+//19                 dist[v] ← alt
+//20                 prev[v] ← u
+//21                 Q.decrease_priority(v, alt)
+//22
+//23     return dist[], prev[]
+func (graph *Graph) Dijkstra(source Id) (dist map[Id]float64, prev map[Id]Id, err error) {
+	if _, exists := graph.vertices[source]; !exists {
+		return nil, nil, fmt.Errorf("Vertex %v is not existed", source)
+	}
+
+	dist = make(map[Id]float64)
+	prev = make(map[Id]Id)
+	heap := fibHeap.NewFibHeap()
+
+	for id := range graph.vertices {
+		prev[id] = nil
+		if id != source {
+			dist[id] = math.Inf(1)
+			heap.Insert(id, math.Inf(1))
+		} else {
+			dist[id] = 0
+			heap.Insert(id, 0)
+		}
+	}
+
+	for heap.Num() != 0 {
+		min, _ := heap.ExtractMin()
+		for to, edge := range graph.egress[min] {
+			if edge.weight < 0 {
+				return nil, nil, fmt.Errorf("Negative weight form vertex %v to vertex %v is not allowed", min, to)
+			}
+			if dist[min]+edge.weight < dist[to] {
+				heap.DecreaseKey(to, dist[min]+edge.weight)
+				prev[to] = min
+				dist[to] = dist[min] + edge.weight
+			}
+		}
+	}
+
+	return
+}
---
+++ dijkstra_test.go
@@ -0,0 +1,93 @@
+// Copyright(c) 2016 Ethan Zhuang <zhuangwj@gmail.com>.
+
+package goraph
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Tests of Dijkstra", func() {
+	var (
+		graph *Graph
+	)
+
+	Context("exception test", func() {
+		BeforeEach(func() {
+			graph = NewGraph()
+		})
+
+		AfterEach(func() {
+			graph = nil
+		})
+
+		It("Given a graph without vertex X, when call dijkstra api with X, then get two nil and error", func() {
+			graph.AddVertex(&myVertex{"S", map[Id]float64{"A": 10, "B": 10}, map[Id]float64{}})
+			graph.AddVertex(&myVertex{"A", map[Id]float64{}, map[Id]float64{"S": 10, "B": 5}})
+			graph.AddVertex(&myVertex{"B", map[Id]float64{"A": 5}, map[Id]float64{"S": 10}})
+
+			dist, prev, err := graph.Dijkstra("X")
+			Expect(dist).Should(BeNil())
+			Expect(prev).Should(BeNil())
+			Expect(err).Should(HaveOccurred())
+		})
+
+		It("Given a graph with negative edge, when call dijkstra api, then get two nil and error", func() {
+			graph.AddVertex(&myVertex{"S", map[Id]float64{"A": 10, "B": 10}, map[Id]float64{}})
+			graph.AddVertex(&myVertex{"A", map[Id]float64{}, map[Id]float64{"S": 10, "B": -5}})
+			graph.AddVertex(&myVertex{"B", map[Id]float64{"A": -5}, map[Id]float64{"S": 10}})
+
+			dist, prev, err := graph.Dijkstra("S")
+			Expect(dist).Should(BeNil())
+			Expect(prev).Should(BeNil())
+			Expect(err).Should(HaveOccurred())
+		})
+	})
+
+	Context("algorithem test", func() {
+		BeforeEach(func() {
+			graph = NewGraph()
+		})
+
+		AfterEach(func() {
+			graph = nil
+		})
+
+		It("Given a non-negative edge graph, when call dijkstra api with source vertex, then get the shortest paths from the source vertices to all other vertex in the graph.", func() {
+			graph.AddVertex(&myVertex{"S", map[Id]float64{"B": 14}, map[Id]float64{"A": 15, "B": 14, "C": 9}})
+			graph.AddVertex(&myVertex{"A", map[Id]float64{"S": 15, "B": 5, "D": 20, "T": 44}, map[Id]float64{"B": 5, "D": 20, "T": 44}})
+			graph.AddVertex(&myVertex{"B", map[Id]float64{"S": 14, "A": 5, "D": 30, "E": 18}, map[Id]float64{"S": 14, "A": 5, "D": 30, "E": 18}})
+			graph.AddVertex(&myVertex{"C", map[Id]float64{"S": 9, "E": 24}, map[Id]float64{"E": 24}})
+			graph.AddVertex(&myVertex{"D", map[Id]float64{"A": 20, "B": 30, "E": 2, "F": 11, "T": 16}, map[Id]float64{"A": 20, "B": 30, "E": 2, "F": 11, "T": 16}})
+			graph.AddVertex(&myVertex{"E", map[Id]float64{"B": 18, "C": 24, "D": 2, "F": 6, "T": 19}, map[Id]float64{"B": 18, "C": 24, "D": 2, "F": 6, "T": 19}})
+			graph.AddVertex(&myVertex{"F", map[Id]float64{"D": 11, "E": 6, "T": 6}, map[Id]float64{"D": 11, "E": 6, "T": 6}})
+			graph.AddVertex(&myVertex{"T", map[Id]float64{"A": 44, "D": 16, "E": 19, "F": 6}, map[Id]float64{"A": 44, "D": 16, "E": 19, "F": 6}})
+			expectedDist := map[Id]float64{
+				"S": 0,
+				"B": 14,
+				"A": 19,
+				"E": 32,
+				"D": 34,
+				"F": 38,
+				"T": 44,
+				"C": 56,
+			}
+			expectedPrev := map[Id]Id{
+				"S": nil,
+				"B": "S",
+				"A": "B",
+				"E": "B",
+				"D": "E",
+				"F": "E",
+				"T": "F",
+				"C": "E",
+			}
+
+			Expect(graph.CheckIntegrity()).ShouldNot(HaveOccurred())
+			dist, prev, err := graph.Dijkstra("S")
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(dist).Should(BeEquivalentTo(expectedDist))
+			Expect(prev).Should(BeEquivalentTo(expectedPrev))
+		})
+	})
+})
